884ab5ac7c89a0d7ae052e57fc83ad57d04aabbe
Astolfen <daniil.zarubin.03@mail.ru>
2023-12-01 14:06:03 +0300

    change setup

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 
-//global variable
-static void *start_head = nullptr;
-static size_t size_alloc = 0;
+struct header {
+    int isUse; // 4 0 не исполь зуется 1 используется
+    void *next;//8
+    void *prev;//8
+    size_t size;//8
+};// mem 36 кратно 4
+
+///global variable
+struct header *start_head;
+static size_t size_alloc;
 
 
 // Эта функция будет вызвана перед тем как вызывать myalloc и myfree
@@ -12,9 +19,12 @@ static size_t size_alloc = 0;
 // buf - указатель на участок логической памяти, который ваш аллокатор
 //       должен распределять, все возвращаемые указатели должны быть
 //       либо равны NULL, либо быть из этого участка памяти
-// size - размер участка памяти, на который указывает buf
+// size - размер участка памяти, на который указывает buf(буду считать что в байтах)
 void mysetup(void *buf, std::size_t size) {
-    start_head = buf;
+    start_head->next = buf;
+    start_head->prev = nullptr;
+    start_head->size = size;
+    start_head->isUse = 0;
     size_alloc = size;
 }
 
@@ -30,6 +40,6 @@ void myfree(void *p) {
 
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << sizeof(int) << std::endl;
     return 0;
 }
