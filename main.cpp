9c580b1f2dd78813bf88fa8308c012b4c7b543b0
Astolfen <daniil.zarubin.03@mail.ru>
2023-12-02 09:57:35 +0300

    change struct header

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 
 struct header {
-    int isUse; // 4 0 не исполь зуется 1 используется
+    unsigned isUse; // 4 0 не исполь зуется 1 используется
     void *next;//8
     void *prev;//8
     size_t size;//8
-};// mem 36 кратно 4
+    unsigned alig=0; // выравнивание до 32
+};// mem 32 byte
 
 ///global variable
-struct header *start_head;
+struct header *start_head; // 8 byte
 static size_t size_alloc;
 
 
@@ -40,6 +41,7 @@ void myfree(void *p) {
 
 
 int main() {
-    std::cout << sizeof(int) << std::endl;
+    header *gsgs;
+    std::cout << sizeof(gsgs) << std::endl;
     return 0;
 }
